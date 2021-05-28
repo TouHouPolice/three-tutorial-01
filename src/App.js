b7216f3ef4d8f71a019850963c0966ac20e19ff5
TouHouPolice <43275328+TouHouPolice@users.noreply.github.com>
2021-05-29 03:51:15 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+import {} from "./static/images"
+import {} from "./static/sounds"
+import {} from "./static/videos"
+
+import Home from "./app/home"
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
